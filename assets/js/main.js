cc439102e869b9e01afe91ee4b54c2a7eda8ff43
Chandan <chandan.b@pacewisdom>
2020-05-15 01:35:28 +0530

    created step-1 wizard with functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 'use strict';
 $(function() {
     // code goes here
+    /*tooltip initialization */
+    $('[data-toggle="tooltip"]').tooltip();
+    /* sal js initialization */
+    sal({
+        threshold: 0.1,
+        once: true,
+    });
 });
